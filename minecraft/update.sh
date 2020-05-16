0fd6e448f4f61d15ef8e9ae736aab2b748402b4e
rafaelurben <rafael.urben@gmail.com>
2020-05-16 17:04:31 +0200

    Updated update.sh

--- minecraft/update.sh
+++ minecraft/update.sh
@@ -14,6 +14,7 @@ wget -q -O auto-restart.sh https://raw.githubusercontent.com/rafaelurben/raspber
 wget -q -O backup-live.sh https://raw.githubusercontent.com/rafaelurben/raspberrypi-minecraftserver/master/minecraft/backup-live.sh
 wget -q -O backup.sh https://raw.githubusercontent.com/rafaelurben/raspberrypi-minecraftserver/master/minecraft/backup.sh
 wget -q -O command.sh https://raw.githubusercontent.com/rafaelurben/raspberrypi-minecraftserver/master/minecraft/command.sh
+wget -q -O restart.sh https://raw.githubusercontent.com/rafaelurben/raspberrypi-minecraftserver/master/minecraft/restart.sh
 wget -q -O start.sh https://raw.githubusercontent.com/rafaelurben/raspberrypi-minecraftserver/master/minecraft/start.sh
 wget -q -O stop.sh https://raw.githubusercontent.com/rafaelurben/raspberrypi-minecraftserver/master/minecraft/stop.sh
 wget -q -O update.sh https://raw.githubusercontent.com/rafaelurben/raspberrypi-minecraftserver/master/minecraft/update.sh
@@ -23,6 +24,7 @@ sudo chmod +x auto-restart.sh
 sudo chmod +x backup-live.sh
 sudo chmod +x backup.sh
 sudo chmod +x command.sh
+sudo chmod +x restart.sh
 sudo chmod +x start.sh
 sudo chmod +x stop.sh
 sudo chmod +x update.sh
