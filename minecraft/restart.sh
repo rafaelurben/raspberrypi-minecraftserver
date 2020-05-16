0c637768e82c4a28674ab7c22c8027d421bf673a
rafaelurben <rafael.urben@gmail.com>
2020-05-16 17:03:48 +0200

    Added missing file

---
+++ minecraft/restart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Startet den Server neu
+
+echo "Führe stop.sh aus..."
+sudo /home/pi/minecraft/stop.sh
+
+echo "Führe start.sh aus..."
+sudo /home/pi/minecraft/start.sh
--- minecraft/start.sh
+++ minecraft/start.sh
@@ -9,7 +9,7 @@ echo "Minecraft Server starten..."
 /usr/bin/screen -dmS minecraft /usr/bin/java -jar -Xms2600M -Xmx2600M /home/pi/minecraft/paperclip.jar
 
 echo "Minecraft Server wird im Hintergrund gestartet!"
-echo "Befehl um in die Konsole zu gelangen: sudo screen -r minecraft."
+echo "Befehl um in die Konsole zu gelangen: sudo screen -r minecraft"
 echo "Um die Konsole wieder zu verlassen, drücke Ctrl+A, danach Ctrl+D"
 
 sudo bash -c "sudo /home/pi/minecraft/auto-restart.sh > /home/pi/minecraft/auto-restart.log 2>&1 &"
